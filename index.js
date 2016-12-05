d195a06aaf9863c52ab7e01760351b02900b4f7a
Manuel BRIOT <briot.manuel@gmail.com>
2016-12-05 23:33:38 +0100

    search return docs list

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./build/decatIndex.min.js');
+module.exports = require('./build/decatIndex.js');
--- test/decatIndexTest.js
+++ test/decatIndexTest.js
@@ -6,7 +6,7 @@ QUnit.test( "search one word that should match", function( assert ) {
 	var results = idx.search("red")
 
 	assert.ok( results.length == "1" )
-	assert.ok( results[0] == "2" )
+	assert.ok( results[0].id == "2" )
 });
 
 QUnit.test( "search one word with no match", function( assert ) {
@@ -25,7 +25,7 @@ QUnit.test( "search with some typing errors that should match", function( assert
 	var results = idx.search("indifine nit foncssion")
 
 	assert.ok( results.length == "1", "Passed!" )
-	assert.ok( results[0] == "1", "Passed!" )
+	assert.ok( results[0].id == "1", "Passed!" )
 });
 
 QUnit.test( "search two words one matching but not the other", function( assert ) {
@@ -45,8 +45,27 @@ QUnit.test( "search two words in three docs with only one matching doc", functio
 
 	var results = idx.search("house mickey")
 
-	assert.ok( results.length == "1")
-	assert.ok( results[0] == "2", "Passed!" )
+	assert.ok( results.length == 1)
+	assert.ok( results[0].id == "2", "Passed!" )
+});
+
+QUnit.test( "should return docs from store", function( assert ) {
+	var idx = new DecatIndex({ref : "testId", fieldsToIndex : ['title'], fieldsToStore : ['title','content','author']})
+	idx.indexDoc({testId:"1",title : "house of pain",content : "a book about java",author : "manu"})
+	idx.indexDoc({testId:"2",title : "house of mickey",content : "a book about css",author : "raphael"})
+	idx.indexDoc({testId:"3",title : "house of mickey",content : "a book about javascript",author : "thèrence"})
+
+	var results = idx.search("house")
+
+	assert.ok( results.length == 3)
+	assert.ok( results[0].testId == "1", "Passed!" )
+	assert.ok( results[0].title == "house of pain", "Passed!" )
+	assert.ok( results[0].content == "a book about java", "Passed!" )
+	assert.ok( results[0].author == "manu", "Passed!" )
+	assert.ok( results[2].testId == "3", "Passed!" )
+	assert.ok( results[2].title == "house of mickey", "Passed!" )
+	assert.ok( results[2].content == "a book about javascript", "Passed!" )
+	assert.ok( results[2].author == "thèrence", "Passed!" )
 });
 
 QUnit.test( "should serialized and rebuild index correctly", function( assert ) {
@@ -67,7 +86,7 @@ QUnit.test( "should serialized and rebuild index correctly", function( assert ) 
 	var results = newIdx.search("house mickey")
 
 	assert.ok( results.length == "1")
-	assert.ok( results[0] == "2", "Passed!" )
+	assert.ok( results[0].id == "2", "Passed!" )
 });
 
 QUnit.test( "should store doc ref in index with gamma transformation", function( assert ) {
@@ -78,5 +97,7 @@ QUnit.test( "should store doc ref in index with gamma transformation", function(
 	idx.indexDoc({id:"12",title : "house"})
 
 	assert.deepEqual(idx.getFromIndex('house'),[1,2,4,5])
-	assert.deepEqual(idx.search("house"),[1,3,7,12])
+	var docs = idx.search("house")
+	var ids = docs.map(function(doc){return doc.id})
+	assert.deepEqual(ids,[1,3,7,12])
 });
